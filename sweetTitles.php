b3df8af6c27d56d17c915cc86369b58aba5a4f89
blueandhack <blueandhack@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-19 20:35:51 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sweettitles/trunk@278444 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- sweetTitles.php
+++ sweetTitles.php
@@ -6,8 +6,7 @@ Plugin Name: sweetTitles
 
 Plugin URI: http://blueandhack.com/wordpress/832.html
 
-Description: The name comes from Sweet Titles to change the Title prompt effects plug-js Sweet Titles Finalized, this plug-in effect makes the title prompted to become beautiful, and can show you are going to click on the link to the url, so that users know where to go, while improving degree of user experience.
-Sweet Titles 这个名字源自于改变 Title 提示效果的 js 插件 Sweet Titles Finalized,此插件使得 title 提示效果变得美观，而且可以显示出你将要点击的链接的 url，让用户知道自己将要去哪里，同时提升了用户感受度。
+Description: The name comes from Sweet Titles to change the Title prompt effects plug-js Sweet Titles Finalized, this plug-in effect makes the title prompted to become beautiful, and can show you are going to click on the link to the url, so that users know where to go, while improving degree of user experience.Sweet Titles 这个名字源自于改变 Title 提示效果的 js 插件 Sweet Titles Finalized,此插件使得 title 提示效果变得美观，而且可以显示出你将要点击的链接的 url，让用户知道自己将要去哪里，同时提升了用户感受度。
 
 Version: 1.1
 
